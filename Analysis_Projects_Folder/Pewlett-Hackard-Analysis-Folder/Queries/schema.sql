0fbece181fde889fa2d40ff164c13de72dafd995
asongulol <otrinidad@abckidsny.com>
2020-07-02 23:30:42 -0400

    created retirement_info.csv table

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis-Folder/Queries/schema.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis-Folder/Queries/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE employees (
 );
 -- join two fields as primary key for example (emp_no, dept_no)
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
@@ -46,17 +46,42 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title)
+	PRIMARY KEY (emp_no, title, from_date)
 );
-
+-- FOLLOW THE LIST OF COLUMNS AS FOUND IN THE CSV FILE TO BE IMPORTED, IT MAKES A DIFFERENCE!!!!
 CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+-- WHEN IMPORTING MAKE SURE TO IMPORT IN THE CORRECT ORDER TO PREVENT ERRORS WHERE A KEY DOES NOT YET EXIST
+-- DROP TABLE "TABLE_NAME" CASCADE; CASCADE ALSO DELETES CONNECTIONS TO OTHER TABLES
+-- Any table that does not reference a foreign key can be dropped without the CASCADE constrain
+SELECT * FROM dept_emp;
 
-SELECT * FROM departments;
+-- FIND OUT RETIRABLE EMPLOYEES 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- FIND OUT COUNT OF RETIRABLE EMPLOYEES 
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- USING "INTO" COMMAND TO CREATE A NEW TABLE
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- SEE WHAT THE NEW TABLE LOOKS LIKE
+
+SELECT * FROM retirement_info;
